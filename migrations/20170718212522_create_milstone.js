7675d863211a90587208362e8fdb3754ba986649
RiptideWallace <brysonc1987@gmail.com>
2017-07-19 01:54:19 +0000

    migrations exercise complete

--- migrations/20170718212522_create_milstone.js
+++ migrations/20170718212522_create_milstone.js
@@ -1,17 +1,13 @@
 
-exports.up = function(knex, Promise) {
-  return Promise.all([
-    knex.schema.createTable('milestones', function(table){
+exports.up = function(knex) {
+  return knex.schema.createTable('milestones' (table) => {
       table.increments('id').primary();
       table.string('description');
       table.date('date_achieved');
       table.foreign('famous_people_id').references('famous_people');
     })
-  ])
 };
 
-exports.down = function(knex, Promise) {
-  return Promise.all([
-    knex.schema.dropTable('milestones')
-  ])
+exports.down = function(knex) {
+  return knex.schema.dropTable('milestones')
 };
